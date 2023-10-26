47cbf4720177c9d74d000a45567376f7dde52221
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-10-26 16:05:13 +0200

    completata elaborazione e output+creato file .class

--- Lab_20_10/Es5.java
+++ Lab_20_10/Es5.java
@@ -3,3 +3,20 @@
  * @author Thomas Burla
  * @version 1.0
  */
+import javax.swing.*;
+public class Es5{
+    public static void main(String args[]){
+        //dichiarazione variabili
+        int a;
+        int b;
+        int c;
+        //elaborazione
+        a = Integer.parseInt(JOptionPane.showInputDialog("Dammi un numero intero"));
+        b = Integer.parseInt(JOptionPane.showInputDialog("Dammi il secondo numero intero"));
+        c=(a);
+        a=(b);
+        b=(c);
+        //output
+        System.out.println("I due valori, una volta invertiti risultano essere nel seguente ordine: "+a+ ";" +b);
+    }
+}
