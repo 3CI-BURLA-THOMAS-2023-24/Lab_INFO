90d92b19f3fcce98a208c46ed1fdfa0f4592eec0
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-10-26 15:19:21 +0200

    dichiarazione e inizializzazione delle variabili

--- Lab_20_10/Es5.java
+++ Lab_20_10/Es5.java
@@ -1,5 +1,16 @@
 /**
- * Dato in ingresso il lato di un triangolo equilatero, visualizzare il suo perimetro
+ * Dato in ingresso il lato di un triangolo equilatero, visualizzare il suo perimetro.
  * @author Thomas Burla
  * @version 1.0
  */
+import javax.swing.*;
+public class Es5{
+    public static void main(String args[]){
+        //dichiarazione delle variabili
+        float lato;
+        float perimetro;
+        //inizializzazione delle variabili
+        lato = 0;
+        perimetro = 0;
+    }
+}
