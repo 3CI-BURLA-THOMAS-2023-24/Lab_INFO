f8fc88643730138670637a59d9bf81cd29278479
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-10-27 10:25:18 +0200

    classe terminata

--- Lab_27_10_if_else_Burla/Es05.java
+++ Lab_27_10_if_else_Burla/Es05.java
@@ -9,6 +9,12 @@ public class Es05{
         //dichiarazione delle variabili
         int num;
         //inizializzazione
-        num = Integer.parseInt(JOptionPane.showInputDialog("Inserire un numero intero per capire se è pari o dispari"));
+        num = Integer.parseInt(JOptionPane.showInputDialog("Inserire un numero intero per capire se è multiplo di 7"));
+        if(num%7==0){
+            System.out.println("Il numero è multiplo di 7.");
+        }
+        else{
+            System.out.println("Il numero non è multiplo di 7.");
+        }
     }
 }
