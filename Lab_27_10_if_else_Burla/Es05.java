187bb596d820e08f67b4d4a706806285f29241b7
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-10-27 10:23:43 +0200

    inizio classe e variabili

---
+++ Lab_27_10_if_else_Burla/Es05.java
@@ -0,0 +1,14 @@
+/**
+ * Letto un intero, dire se esso è multiplo di 7.
+ * @author Thomas Burla
+ * @version 1.0
+ */
+import javax.swing.*;
+public class Es05{
+    public static void main(String args[]){
+        //dichiarazione delle variabili
+        int num;
+        //inizializzazione
+        num = Integer.parseInt(JOptionPane.showInputDialog("Inserire un numero intero per capire se è pari o dispari"));
+    }
+}
