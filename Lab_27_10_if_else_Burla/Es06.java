8f87bd69e9c8120c52608790bce31fa52b3c8cbb
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-10-27 10:30:35 +0200

    creo classe, dichiaro e inizializzo variabili

---
+++ Lab_27_10_if_else_Burla/Es06.java
@@ -0,0 +1,19 @@
+/**
+ * Stabilire se tre numeri presi in ingresso possono essere le misure dei lati di un triangolo.
+ * @author Thomas Burla
+ * @version 1.0
+ */
+import javax.swing.*;
+public class Es06{
+    public static void main(String args[]){
+        //dichiarazione delle variabili
+        double l1;
+        double l2;
+        double l3;
+        //inizializzazione delle avriabili
+        l1 = Double.parseDouble(JOptionPane.showInputDialog("Inserire il primo lato del triangolo"));
+        l2 = Double.parseDouble(JOptionPane.showInputDialog("Inserire il secondo lato del triangolo"));
+        l3 = Double.parseDouble(JOptionPane.showInputDialog("Inserire il tero lato del triangolo"));
+        
+    }
+}
