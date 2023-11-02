61af5e14d07303959935512be67ecf4bc9dd0dd4
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-11-02 17:36:24 +0100

    classe completata

--- Lab_27_10_if_else_Burla/Es09.java
+++ Lab_27_10_if_else_Burla/Es09.java
@@ -10,5 +10,13 @@ public class Es09{
         double voto;
         //inizializzazione delle variabili
         voto = Double.parseDouble(JOptionPane.showInputDialog("Inserire il voto per stabilire se è insuffieciente, sufficiente o più che sufficiente."));
+        //elaborazione
+        if(voto < 6){
+            System.out.println("Il voto non è sufficiente.");
+        }else if(voto < 8){
+            System.out.println("Il voto è sufficiente.");
+        }else{
+            System.out.println("Il voto inserito è più che sufficiente.");
+        }
     }
 }
