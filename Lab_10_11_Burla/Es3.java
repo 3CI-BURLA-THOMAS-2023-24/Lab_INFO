b2e4dd3d7ed0d8c5c1ca2d5f797f7ac7db71ff54
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-11-10 10:47:53 +0100

    progetto completato

--- Lab_10_11_Burla/Es3.java
+++ Lab_10_11_Burla/Es3.java
@@ -7,11 +7,19 @@ import javax.swing.*;
 public class Es3{
     public static void main(String args []){
         //dichiarazione delle variabili
-        int x, y, i;
+        int x, y, i, p;
         //inizializzazione delle variabili
+        p = 0;
+        //controllo dell'input di x e y
         do{
             x = Integer.parseInt(JOptionPane.showInputDialog("Inserire il numero intero positivo da moltiplicare"));
             y = Integer.parseInt(JOptionPane.showInputDialog("Quante volte si vuole moltiplicare tale numero per se stesso? Indicare solo numeri interi positivi"));
         }while((x <= 0) || (y <=0));
+        //elaborazione
+        for(i = 0; i < y; i++){
+            p = p + x;
+        }
+        //output
+        JOptionPane.showMessageDialog(null, "Il risultato è: "+p);
     }
 }
