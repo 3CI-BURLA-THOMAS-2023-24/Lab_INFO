30cc45388e5a1e3f9ae66a21ad8fafc064aeb58e
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-11-10 10:28:12 +0100

    bug fixing

--- Lab_10_11_Burla/Es2.java
+++ Lab_10_11_Burla/Es2.java
@@ -7,12 +7,25 @@ import javax.swing.*;
 public class Es2{
     public static void main(String args[]){
         //dichiarazione delle variabili
-        int n, x, p;
+        int n, x, p, i;
         //inizializzazone delle variabili
         p = 1;
         //controllo dell'input per il numero di valori da moltiplicare
         do{
             n = Integer.parseInt(JOptionPane.showInputDialog("Quanti sono i valori da moltiplicare?"));
         }while(n < 0);
+        //elaborazione
+        for(i = 0; i < n; i++){
+            x = Integer.parseInt(JOptionPane.showInputDialog("Inserire un valore da moltiplicare"));
+            //se un numero vale 0, il prodotto vale 0 a prescindere
+            if(x == 0){
+                p = 0;
+                i = n;
+            }else{
+            p = p * n;
+            }
+        }
+        //output
+        JOptionPane.showMessageDialog(null, "Il prodotto è "+p);
     }
 }
