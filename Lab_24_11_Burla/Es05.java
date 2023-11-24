34dfa724635e6eeba791436b1d29bc4c4940944b
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-11-24 10:52:51 +0100

    classe terminata

--- Lab_24_11_Burla/Es05.java
+++ Lab_24_11_Burla/Es05.java
@@ -7,5 +7,21 @@ import javax.swing.*;
 public class Es05{
     public static void main(String args[]){
         //dichiarazione delle variabili
+        int x, y, s, i;
+        //inizializzazione delle variabili
+        x = 220;
+        y = 284;
+        s = 0;
+        //elaborazione
+        for(i = 1; i <= y; i++){
+            if((y % i) == 0){
+                s = s + i;
+            }
+        }
+        if(s == x){
+            JOptionPane.showMessageDialog(null, "I due numeri sono amici");
+        }else{
+            JOptionPane.showMessageDialog(null, "I due numeri NON sono amici");
+        }
     }
 }
