b2cbd51677c037440fdf0f46e16ac068cd6f9db6
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-11-24 10:37:00 +0100

    classe creata

---
+++ Lab_24_11_Burla/Es05.java
@@ -0,0 +1,11 @@
+/**
+ * Calcolo della circonferenza e area del cerchio, con il tipo di dato opportuno per il raggio e il controllo sul valore espresso in input;
+ * @author Thomas Burla 
+ * @version 1.0
+ */
+import javax.swing.*;
+public class Es05{
+    public static void main(String args[]){
+        //dichiarazione delle variabili
+    }
+}
