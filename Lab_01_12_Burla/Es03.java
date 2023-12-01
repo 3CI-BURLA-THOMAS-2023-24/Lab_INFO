cc442b3ebdbc586b875c63af7e8cc9536c1dc44d
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-12-01 09:38:10 +0100

    es03

---
+++ Lab_01_12_Burla/Es03.java
@@ -0,0 +1,18 @@
+/**
+ * Visualizzare l’ultima cifra di un numero intero inserito se pari.
+ * @author Thomas Burla
+ * @version 1.0
+ */
+import javax.swing.*;
+public class Es03{
+    public static void main(String args[]){
+        //dichiarazione delle variabili
+        int val, resto;
+        //inizializzazione delle variabili
+        val = Integer.parseInt(JOptionPane.showInputDialog("Inserire un valore intero per visualizzare la sua ultima cifra, se pari"));
+        //elaborazione
+        resto = val % 10;
+        //output
+        JOptionPane.showMessageDialog(null, "L'ultima cifra è: "+resto);
+    }
+}
