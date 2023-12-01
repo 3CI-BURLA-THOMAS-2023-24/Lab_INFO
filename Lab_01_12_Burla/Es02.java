d4a7a41d89126fbc8e3909d6d3cd75fd954cbae1
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2023-12-01 09:28:02 +0100

    es02

--- Lab_01_12_Burla/Es02.java
+++ Lab_01_12_Burla/Es02.java
@@ -1,11 +1,26 @@
-
 /**
  * Letti n  numeri interi (3<n<14) , calcolare la somma dei valori pari.
  * @author Thomas Burla
  * @version 1.0
  */
+import javax.swing.*;
 public class Es02{
     public static void main(String args[]){
-        //
+        //dichiarazione delle variabili
+        int n, val, somma;
+        somma = 0;
+        do{
+            n = Integer.parseInt(JOptionPane.showInputDialog("Quanti valori vuoi inserire? Minimo 4 e massimo 13"));
+            if((n <= 3) || (n >= 14)){
+                JOptionPane.showMessageDialog(null, "Errore! Inserire solo numeri maggiori di 3 e minori di 14");
+            }
+        }while((n <= 3) || (n >= 14));
+        val = Integer.parseInt(JOptionPane.showInputDialog("Inserire un valore per visualizzare la somma tra i numeri pari"));
+        for(int i = 0; i < n; i++){
+            if((val % 2) == 0){
+                somma = somma + val;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "La somma dei numeri pari che hai inserito è: "+somma);
     }
 }
