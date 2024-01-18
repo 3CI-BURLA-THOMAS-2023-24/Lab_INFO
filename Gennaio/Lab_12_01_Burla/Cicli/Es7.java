1d7e957663089f49ca5c0b87299f0f696b294ea7
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-01-18 16:24:02 +0100

    Create Es7.java

---
+++ Gennaio/Lab_12_01_Burla/Cicli/Es7.java
@@ -0,0 +1,20 @@
+/**
+ * Scrivere un programma che legga un carattere finché l’utente digita ‘s’ oppure ‘n’. In questo caso stampa OK.
+ * 
+ * @author Burla Thomas
+ * @version 1.0
+ */
+import javax.swing.*;
+public class Es7{
+    public static void main(String args[]){
+        //dichiarazione delle variabili
+        String carattere;
+        //controllo dell'input
+        do{
+            carattere = JOptionPane.showInputDialog("Inserire un carattere. Per far terminare il programma, digitare s oppure n");
+            if((carattere.equals("s")) || (carattere.equals("n"))){
+                JOptionPane.showMessageDialog(null, "OK");
+            }
+        }while((carattere.equals("s")) || (carattere.equals("n")));
+    }
+}
