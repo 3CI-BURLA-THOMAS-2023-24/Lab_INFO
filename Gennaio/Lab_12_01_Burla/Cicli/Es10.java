bea84c769c273aac5fe7a945e1ff027663ea6d55
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-01-19 09:38:05 +0100

    classe Es10 iniziata

---
+++ Gennaio/Lab_12_01_Burla/Cicli/Es10.java
@@ -0,0 +1,13 @@
+/**
+ * Si scriva un programma per convertire un numero binario in decimale e viceversa.
+ * 
+ * @author Burla Thomas
+ * @version 1.0
+ */
+import javax.swing.JOptionPane;
+public class Es10{
+    public static void main(String args[]){
+        //dichiarazione delle variabili
+        
+    }
+}
