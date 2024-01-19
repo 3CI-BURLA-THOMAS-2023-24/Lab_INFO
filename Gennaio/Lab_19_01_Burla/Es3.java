ebc0e55e01c17a39bf90e906e1830562fd516a8b
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-01-19 10:50:28 +0100

    classe Es3 iniziata

---
+++ Gennaio/Lab_19_01_Burla/Es3.java
@@ -0,0 +1,19 @@
+/**
+ * Presa una stringa, modificarla in modo che il primo carattere sia ripetuto una volta, il secondo due, il terzo tre, e così via.
+ * 
+ * @author Burla Thomas
+ * @version 1.0
+ */
+import javax.swing.JOptionPane;
+public class Es3{
+    public static void main(String args[]){
+        //dichiarazione delle variabili
+        String input, output;
+        //inizializzazione variabili
+        input = JOptionPane.showInputDialog("Inserire una stringa per visualizzare una nuova stringa con il primo carattere ripetuto una volta, il secondo due, il terzo tre, e così via");
+        output = "";
+        for(int i = 0; i <= input.length(); i++){
+            
+        }
+    }
+}
