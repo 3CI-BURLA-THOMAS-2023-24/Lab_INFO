c1b32ea61223adcaf4bd8384f272e6e548eb3206
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-01-21 12:36:16 +0100

    classe Es4 iniziata

---
+++ Gennaio/Lab_19_01_Burla/Es4.java
@@ -0,0 +1,17 @@
+/**
+ * Contare da quante parole è composta una stringa.
+ * 
+ * @author Burla Thomas
+ * @version 1.0
+ */
+import javax.swing.JOptionPane;
+public class Es4{
+    public static void main(String args[]){
+        //dichiarazione delle variabili
+        String stringa;
+        int counter;
+        //inizializzazione delle variabili
+        stringa = JOptionPane.showInputDialog("Inserire una stringa per contare da quante parole è composta");
+        counter = 0;
+    }
+}
