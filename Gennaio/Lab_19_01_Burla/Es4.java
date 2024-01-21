22494db2016868dc6ee82aa6e8c3976686480d6a
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-01-21 13:18:36 +0100

    Update Es4.java

--- Gennaio/Lab_19_01_Burla/Es4.java
+++ Gennaio/Lab_19_01_Burla/Es4.java
@@ -12,7 +12,14 @@ public class Es4{
         int counter;
         char spazio = ' ';
         //inizializzazione delle variabili
-        stringa = JOptionPane.showInputDialog("Inserire una stringa per contare da quante parole è composta");
+        //controllo se la stringa è vuota
+        do{
+            stringa = JOptionPane.showInputDialog("Inserire una stringa per contare da quante parole è composta");
+            //messaggio di errore se la stringa è vuota
+            if(stringa.equals("")){
+                JOptionPane.showMessageDialog(null, "ERRORE! Inserire almeno una parola");
+            }
+        }while(stringa.equals(""));
         counter = 1;
         //ripeto le operazioni finche non ho letto tutti i carattteri della stringa
         for(int i = 0; i < stringa.length(); i++){
