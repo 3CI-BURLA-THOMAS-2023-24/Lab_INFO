836356cdbcecd37cd20fcfe5b74eaf1f4e1d0d10
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-01-21 12:50:08 +0100

    classe Es11 terminata

---
+++ Gennaio/Lab_19_01_Burla/Es11.java
@@ -0,0 +1,27 @@
+/**
+ * 11. Scrivere un programma che realizzi la r moscia: in una stringa, sostituire tutte le ‘r’ con ‘v’.
+ * 
+ * @author Burla Thomas
+ * @version 1.0
+ */
+import javax.swing.JOptionPane;
+public class Es11{
+    public static void main(String args[]){
+        //dichiarazione delle variabili
+        String input, output;
+        char erre = 'r';
+        input = JOptionPane.showInputDialog("Inserire una stringa per sostituire le r con le v");
+        output = "";
+        //ripeto le operazioni finche non ho letto tutti i carattteri della stringa
+        for(int i = 0; i < input.length(); i++){
+            //se il carattere in posizione i vale r, allora lo sostituisco. In caso contrario, lo trascrivo uguale a com'è nella stringa
+            if((input.charAt(i)) == erre){
+                output = output + 'v';
+            }else{
+                output = output + input.charAt(i);
+            }
+        }
+        //output
+        JOptionPane.showMessageDialog(null, output);
+    }
+}
