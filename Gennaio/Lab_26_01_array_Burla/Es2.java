e711008a8f7a4d926731d3c5ee366e46a122d484
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-01-26 10:53:39 +0100

    errore da sistemare

---
+++ Gennaio/Lab_26_01_array_Burla/Es2.java
@@ -0,0 +1,56 @@
+/**
+ *  • Leggere un valore l compreso tra 3 e 10
+    • leggere un vettore di l valori double corrispondenti ai voti di un alunno (voto compreso tra 3 e 10)
+    • calcolare e visualizzare la media dei voti
+    • dire se l'alunno è promosso o bocciato
+    • visualizzare il voto più basso e il voto più alto
+ * 
+ * @author Thomas Burla
+ * @version 1.0
+ */
+import javax.swing.JOptionPane;
+public class Es2{
+    public static void main(String args[]){
+        //dichiarazione variabili
+        int l;
+        double voto[], media, max, min, somma;
+        //inizializzazione variabili
+        media = somma = max = 0.0;
+        min = 10.0;
+        //leggo l e controllo l'input
+        do{
+            l = Integer.parseInt(JOptionPane.showInputDialog("Quanti voti desideri inserire? Minimo 3 e massimo 10"));
+            if((l < 3) || (l > 10)){
+                JOptionPane.showMessageDialog(null, "ERRORE! Inserire un valore rispettando le indicazioni");
+            }
+        }while((l < 3) || (l > 10));
+        //allocazione array
+        voto = new double[l];
+        //leggo vettore con un ciclo
+        for(int i = 0; i < l; i++){
+            //controllo input
+            do{
+                voto[i] = Double.parseDouble(JOptionPane.showInputDialog("Inserire un voto compreso tra 3 e 10"));
+            }while((voto[i] < 3) || (voto[i] > 10));
+            //calcolo la somma, usata poi per la media
+            somma = somma + voto[i];
+            if((i != 0) && (i != (l))){
+                if(voto[i] > voto [i-1]){
+                    max = voto[i];
+                }else{
+                    min = voto[i];
+                }
+            }
+        }
+        //calcolo media
+        media = somma / l;
+        //output
+        if(media < 6){
+            JOptionPane.showMessageDialog(null, "Bocciato");
+        }else{
+            JOptionPane.showMessageDialog(null, "Promosso");
+        }
+        JOptionPane.showMessageDialog(null, "Voto più basso-->"+min);
+        JOptionPane.showMessageDialog(null, "Voto più alto-->"+max);
+    }
+}
