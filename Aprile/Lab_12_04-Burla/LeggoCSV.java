0e98998a79300ac5efd2128b9ebe4f8dacebbf36
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-04-18 17:10:26 +0200

    terminata classe

--- Aprile/Lab_12_04-Burla/LeggoCSV.java
+++ Aprile/Lab_12_04-Burla/LeggoCSV.java
@@ -1,16 +1,20 @@
 /**
- * lettura di un file: cerco parola in un file CSV
+ * lettura di un file: carico in una matrice i voti degli studenti e in un array il loro nome file CSV
  * 
  * @author Thomas Burla
  * @version 1.0
  */
 import java.io.*;
 import javax.swing.JOptionPane;
-import java.util.Scanner;
+import java.util.*;
 public class LeggoCSV{
     public static void main(String args[]){
         //dichiarazione e inizializzazione variabili
-        String pathname, parola;
+        String pathname;
+        String riga[];
+        ArrayList <String> nomi = new ArrayList <String>();
+        ArrayList <double[]> voti = new ArrayList <double[]>();
+        double votiInput[];
         //leggo il nome del file
         do{
             pathname = JOptionPane.showInputDialog("Inserire il nome del file, estensione compresa. Nel caso in cui il file sia in una posizione diversa rispetto al programma, indicare l'indirizzo assoluto.");
@@ -18,13 +22,29 @@ public class LeggoCSV{
                 JOptionPane.showMessageDialog(null, "ERRORE! Stringa vuota", "Errore", JOptionPane.ERROR_MESSAGE);
             }
         }while((pathname.equals("")) || (pathname.equals(" ")));
-        //leggo la praola da cercare
-        do{
-            parola = JOptionPane.showInputDialog("Inserire la parola che si vuole cercare nel file");
-            if((parola.equals("")) || (parola.equals(" "))){
-                JOptionPane.showMessageDialog(null, "ERRORE! Stringa vuota", "Errore", JOptionPane.ERROR_MESSAGE);
+        try{
+            //preparo lettura
+            File f = new File(pathname);
+            FileReader fr = new FileReader(f);
+            Scanner leggo = new Scanner(fr);
+            //leggo righe da file
+            while(leggo.hasNextLine()){
+                riga = (leggo.nextLine()).split(",");
+                //alloco array dei voti del singolo studente
+                votiInput = new double[(riga.length) - 1];
+                //salvo nella lista dei nomi il nome dello studente, che si trova nella prima posizione
+                nomi.add(riga[0]);
+                //riempio un array con i voti del suddetto studente, dopo aver convertito in double i voti salvati nel vettore riga
+                for(int i = 1; i < riga.length;i++){
+                    votiInput[i - 1] = Double.parseDouble(riga[i]);
+                }
+                //salvo array dei voti dello studente sulla matrice dinamica che conterrà tutti i voti
+                voti.add(votiInput);
             }
-        }while((parola.equals("")) || (parola.equals(" ")));
-        
+            //chiudo flusso
+            leggo. close();
+        }catch(IOException e){
+            JOptionPane.showMessageDialog(null, "ERRORE di I/O, file NON raggiungibile", "Errore di I/O", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
