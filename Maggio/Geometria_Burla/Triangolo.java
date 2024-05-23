317516faf4cbb8e20e55a16eeca2dbfd55e9adba
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-05-23 17:36:56 +0200

    aggiunti metodi
    METODI AGGIUNTI:
    1)calcolaArea: per calcolare l'area dato in input il lato che si considera base;
    2)calcolaPerimetro: per claoclare il perimetro in base al tipo di triangolo

--- Maggio/Geometria_Burla/Triangolo.java
+++ Maggio/Geometria_Burla/Triangolo.java
@@ -117,29 +117,32 @@ public class Triangolo {
     public void setLato1(double lato1){
         if(lato1 > 0.0){
             this.lato1 = lato1;
+            this.calcolaTipo();
         }
     }
     //get lato1
     public double getLato1(){
         return this.lato1;
     }
-    //set lato1
+    //set lato2
     public void setLato2(double lato2){
         if(lato2 > 0.0){
             this.lato2 = lato2;
+            this.calcolaTipo();
         }
     }
-    //get lato1
+    //get lato2
     public double getLato2(){
         return this.lato2;
     }
-    //set lato1
+    //set lato3
     public void setLato3(double lato3){
         if(lato3 > 0.0){
             this.lato3 = lato3;
+            this.calcolaTipo();
         }
     }
-    //get lato1
+    //get lato3
     public double getLato3(){
         return this.lato3;
     }
@@ -173,28 +176,53 @@ public class Triangolo {
     public double getPerimetro(){
         return this.perimetro;
     }
-    //calcolo perimetro, controllando i lati e calcolando il tipo
-    public double calcolaPerimetro(){
-        if((this.lato1 > 0.0) && (this.lato2 > 0.0) && (this.lato3 > 0.0)){
-            if((this.lato1 == this.lato2) || (this.lato1 == this.lato3) || (this.lato2 == this.lato3)){
-                if((this.lato1 == this.lato2) && (this.lato1 == this.lato3)){
+    //calcolo tipo
+    public void calcolaTipo(){
+        if((lato1 > 0.0) && (lato2 > 0.0) && (lato3 > 0.0)){
+            if((lato1 == lato2) || (lato1 == lato3) || (lato2 == lato3)){
+                if((lato1 == lato2) && (lato1 == lato3)){
                     this.tipo = "equilatero";
-                    this.perimetro = lato1 * 3;
                 }else{
                     this.tipo = "isoscele";
-                    if(this.lato1 == this.lato2){
-                        this.perimetro = this.lato1 * 2 + this.lato3;
-                    }else if(this.lato1 == this.lato3){
-                        this.perimetro = this.lato1 * 2 + this.lato2;
-                    }else if(this.lato2 == this.lato3){
-                        this.perimetro = this.lato2 * 2 + this.lato1;
-                    }
                 }
             }else{
                 this.tipo = "scaleno";
+            }
+        }
+    }
+    //calcolo perimetro, controllando i lati e calcolando il tipo
+    public double calcolaPerimetro(){
+        if(this.tipo != "-"){
+            if((this.tipo).equals("equilatero")){
+                this.perimetro = lato1 * 3;
+            }else if((this.tipo).equals("isoscele")){
+                if(this.lato1 == this.lato2){
+                    this.perimetro = this.lato1 * 2 + this.lato3;
+                }else if(this.lato1 == this.lato3){
+                    this.perimetro = this.lato1 * 2 + this.lato2;
+                }else if(this.lato2 == this.lato3){
+                    this.perimetro = this.lato2 * 2 + this.lato1;
+                }
+            }else if((this.tipo).equals("scaleno")){
                 this.perimetro = this.lato1 + this.lato2 + this.lato3;
             }
             return this.perimetro;
+        }
+        return -1;
+    }
+    //calcolo area, controllando base e altezza
+    public double calcolaArea(String baseTriangolo){
+        double base = 0.0;
+        if(baseTriangolo.equalsIgnoreCase("lato1")){
+            base = this.lato1;
+        }else if(baseTriangolo.equalsIgnoreCase("lato2")){
+            base = this.lato2;
+        }else if(baseTriangolo.equalsIgnoreCase("lato3")){
+            base = this.lato3;
+        }
+        if(base != 0.0){
+            this.area = base * this.altezza;
+            return this.area;
         }
         return -1;
     }
