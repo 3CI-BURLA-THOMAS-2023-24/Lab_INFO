dbf32cf43f8cbf12fc0a334ec1efed6ea472574b
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-05-26 17:43:25 +0200

    aggiunta arraylist di Attore

--- Maggio/Prenotazioni_teatro_Burla/Attore.java
+++ Maggio/Prenotazioni_teatro_Burla/Attore.java
@@ -7,25 +7,41 @@
 public class Attore {
     //variabili d'istanza
     private String nome;
+    private String cognome;
     private int anni;
+    private String parte;
     //costruttore senza parametri
     public Attore(){
         this.nome = "-";
+        this.cognome = "-";
         this.anni = 0;
+        this.parte = "-";
     }
     //costruttore con parametri controllati
-    public Attore(String nome, int anni){
+    public Attore(String nome, String cognome, int anni, String parte){
         //nome
         if(nome != null){
             this.nome = nome;
         }else{
             this.nome = "-";
         }
+        //cognome
+        if(cognome != null){
+            this.cognome = cognome;
+        }else{
+            this.cognome = "-";
+        }
         //anni
         if(anni > 0){
             this.anni = anni;
         }else{
             anni = 0;
+        }
+        //parte
+        if(parte != null){
+            this.parte = parte;
+        }else{
+            this.parte = "-";
         }
     }
     //set nome
@@ -38,6 +54,16 @@ public class Attore {
     public String getNome(){
         return this.nome;
     }
+    //set cognome
+    public void setCognome(String cognome){
+        if(cognome != null){
+            this.cognome = cognome;
+        }
+    }
+    //get cognome
+    public String getCognome(){
+        return this.cognome;
+    }
     //set anni
     public void setAnni(int anni){
         if(anni > 0){
@@ -48,11 +74,22 @@ public class Attore {
     public int getAnni(){
         return this.anni;
     }
+    //set parte
+    public void setparte(String parte){
+        if(parte != null){
+            this.parte = parte;
+        }
+    }
+    //get parte
+    public String getparte(){
+        return this.parte;
+    }
     //toString
     public String toString(){
         String out = "";
-        out+= "L'attore si chiama: " + this.nome;
-        out+= " e ha " + this.anni + "anni";
+        out+= "L'attore si chiama: " + this.nome +  " " + this.cognome;
+        out+= ", ha " + this.anni + "anni";
+        out+= " e ricopre la parte di " + this.parte + ".";
         return out;
     }
 }
