8fb2acede44a5c9496ffcc2b94642b8226e0d3df
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-05-26 17:48:16 +0200

    Update Spettacolo.java

--- Maggio/Prenotazioni_teatro_Burla/Spettacolo.java
+++ Maggio/Prenotazioni_teatro_Burla/Spettacolo.java
@@ -3,23 +3,29 @@
     -nomeSpettacolo
     -durata
     -metodi opportuni
+
+    Modificare le seguenti classi:
+    Spettacolo aggiungere un arrayList di attori
  * 
  * @author Thomas Burla
- * @version 1.0
+ * @version 2.0
  */
+import java.util.*;
 public class Spettacolo {
     //variabili d'istanza
     private String nomeSpettacolo;
     private double durata;
     private String categoria;
+    private ArrayList<Attore> attori;
     //costruttore senza parametri
     public Spettacolo(){
         this.nomeSpettacolo = "-";
         this.durata = 0.0;
         this.categoria = "-";
+        this.attori = null;
     }
     //costruttore con parametri controllati
-    public Spettacolo(String nomeSpettacolo, double durata, String categoria){
+    public Spettacolo(String nomeSpettacolo, double durata, String categoria, Attore attore){
         //nomeSpettacolo
         if(nomeSpettacolo != null){
             this.nomeSpettacolo = nomeSpettacolo;
@@ -37,6 +43,12 @@ public class Spettacolo {
             this.categoria = categoria;
         }else{
             this.categoria = "-";
+        }
+        //attori
+        if(attore != null){
+            (this.attori).add(attore);
+        }else{
+            this.attori = null;
         }
     }
     //set nomeSpettacolo
@@ -69,12 +81,39 @@ public class Spettacolo {
     public String getCategoria(){
         return this.categoria;
     }
+    //set attore
+    public void setAttore(Attore attore){
+        if(attore != null){
+            (this.attori).add(attore);
+        }
+    }
+    //get attore
+    public String getAttore(String nome, String cognome){
+        int i = 0;
+        boolean trovato = false;
+        Attore attore = null;
+        while((i < (this.attori.size())) && (trovato == false)){
+            attore = this.attori.get(i);
+            if((attore.getNome().equalsIgnoreCase(nome)) && (attore.getCognome().equalsIgnoreCase(cognome))){
+                trovato = true;
+            }
+            i++;
+        }
+        if(trovato == true){
+            return attore.toString();
+        }
+        return "Nessun attore corrispondente alle informazioni fornite";
+    }
     //toString
     public String toString(){
         String out = "";
         out+= "Il nome dello spettacolo è: " + this.nomeSpettacolo;
         out+= ".\nLa durata dello spettacolo è: " + this.durata;
         out+= ".\nLa categoria a cui appartiene lo spettacolo è: " + this.categoria;
+        out+= ". A questo spettacolo appartengono gli attori: ";
+        for(int i = 0; i < attori.size(); i++){
+            out+= "\n" + (attori.get(i)).toString();
+        }
         return out;
     }
 }
