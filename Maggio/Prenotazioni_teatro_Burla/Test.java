c08ed24c1c46be7be9aec98d4413d253caa1179b
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-05-29 18:52:48 +0200

    Update Test.java

--- Maggio/Prenotazioni_teatro_Burla/Test.java
+++ Maggio/Prenotazioni_teatro_Burla/Test.java
@@ -11,14 +11,29 @@ import javax.swing.JOptionPane;
 public class Test {
     public static void main(String args[]){
         //creo teatro
-        Teatro ristoriVR = new Teatro("Teatro Ristori", "Verona", 496);
-        //creo i tre spettacoli 
+        Teatro marconiVR = new Teatro("Teatro Ristori", "Verona", 496);
+        //creo i tre spettacoli e li aggiungo al teatro
         Spettacolo jazz = new Spettacolo("Jazz", 90, "Musica");
-        Spettacolo danza = new Spettacolo("Danza", 60, "Musica");
-        Spettacolo serateAutore = new Spettacolo("Serate d'autore", 180, "Incontri");
-        //autori spettacolo jazz
+        marconiVR.setSpettacolo(jazz);
+        Spettacolo commedia = new Spettacolo("Divinaa Commedia", 60, "Letteratura");
+        marconiVR.setSpettacolo(commedia);
+        Spettacolo reggaeton = new Spettacolo("Reggaeton", 180, "Musica");
+        marconiVR.setSpettacolo(reggaeton);
+        //autori spettacolo jazz, aggiunti al rispettivo spettacolo
         Attore patitucci = new Attore("John", "Patitucci", 65, "Bassista");
+        jazz.setAttore(patitucci);
         Attore calderazzo = new Attore("Joseph Dominik", "Calderazzo", 59, "Pianista");
+        jazz.setAttore(calderazzo);
         Attore weckl = new Attore("Dave", "Weckl", 64, "Batterista");
+        jazz.setAttore(weckl);
+        //attori spettacolo commedia
+        Attore dante = new Attore("Dante", "Alighieri", 35, "Protagonista");
+        commedia.setAttore(dante);
+        Attore virgilio = new Attore("Virgilio", "Marone", 60, "Guida");
+        commedia.setAttore(virgilio);
+        //attori spettacolo reggaeton
+        Attore lamborghini = new Attore("Elettra", "Lamborghini", 30, "Cantante");
+        reggaeton.setAttore(lamborghini);
+
     }
 }
