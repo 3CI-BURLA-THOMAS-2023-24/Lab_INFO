4c36957419fca7cf39e93d70fc4416be3665d4f5
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2024-05-30 17:42:10 +0200

    Update Test.java
    creazione di una ArrayList di prenotazioni con i relativi clienti e biglietti

--- Maggio/Prenotazioni_teatro_Burla/Test.java
+++ Maggio/Prenotazioni_teatro_Burla/Test.java
@@ -7,11 +7,20 @@
  * @author Thomas Burla
  * @version 1.0
  */
+import java.util.*;
 import javax.swing.JOptionPane;
 public class Test {
     public static void main(String args[]){
+        //dichiarazione e inizializzazione variabili
+        int ripeti = 0;
+        String nome = "";
+        String cognome = "";
+        String email = "";
+        int anni = 0;
+        ArrayList <Prenotazione> prenotazioni = new ArrayList <Prenotazione> ();
+        ArrayList <Cliente> clienti = new ArrayList <Cliente> ();
         //creo teatro
-        Teatro marconiVR = new Teatro("Teatro Ristori", "Verona", 496);
+        Teatro marconiVR = new Teatro("Teatro Ristori", "Verona", 1800);
         //creo i tre spettacoli e li aggiungo al teatro
         Spettacolo jazz = new Spettacolo("Jazz", 90, "Musica");
         marconiVR.setSpettacolo(jazz);
@@ -34,6 +43,42 @@ public class Test {
         //attori spettacolo reggaeton
         Attore lamborghini = new Attore("Elettra", "Lamborghini", 30, "Cantante");
         reggaeton.setAttore(lamborghini);
+        //ciclo per salvare le prenotazioni su una arraylist
+        do{
 
+            //ciclo per clienti e biglietti
+            do{
+                //controllo nome
+                do{
+                    nome = JOptionPane.showInputDialog("Inserire il nome del cliente");
+                    if((nome.equals("")) || (nome.equals(" "))){
+                        JOptionPane.showMessageDialog(null, "ERRORE! Stringa vuota", "Errore", JOptionPane.ERROR_MESSAGE);
+                    }
+                }while((nome.equals("")) || (nome.equals(" ")));
+                //controllo cognome
+                do{
+                    cognome = JOptionPane.showInputDialog("Inserire il cognome del cliente");
+                    if((cognome.equals("")) || (cognome.equals(" "))){
+                        JOptionPane.showMessageDialog(null, "ERRORE! Stringa vuota", "Errore", JOptionPane.ERROR_MESSAGE);
+                    }
+                }while((cognome.equals("")) || (cognome.equals(" ")));
+                //controllo email
+                do{
+                    email = JOptionPane.showInputDialog("Inserire l'email del cliente");
+                    if((email.equals("")) || (email.equals(" "))){
+                        JOptionPane.showMessageDialog(null, "ERRORE! Stringa vuota", "Errore", JOptionPane.ERROR_MESSAGE);
+                    }
+                }while((email.equals("")) || (email.equals(" ")));
+                //controllo anni
+                do{
+                    anni = Integer.parseInt(JOptionPane.showInputDialog("Inserire l'eetà del cliente"));
+                    if(anni <= 0){
+                        JOptionPane.showMessageDialog(null, "ERRORE! Valore non valido", "Errore", JOptionPane.ERROR_MESSAGE);
+                    }
+                }while(anni <= 0);
+                Cliente cliente = new Cliente(nome, cognome, email, anni);
+
+            }while(ripeti == JOptionPane.YES_OPTION);
+        }while(ripeti == JOptionPane.YES_OPTION);
     }
 }
